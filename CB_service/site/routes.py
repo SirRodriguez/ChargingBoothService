b8fe53c8aae2d1f90ffa45d5adb5f74d629cb116
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-10-05 23:28:56 -0700

    added get sessions route for site

--- CB_service/site/routes.py
+++ CB_service/site/routes.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, join
 import secrets
 from CB_service import db
-from CB_service.models import User, Device, Settings
+from CB_service.models import User, Device, Settings, Session
 
 site = Blueprint('site', __name__)
 
@@ -313,3 +313,57 @@ def upload_images(id):
 	resp = jsonify(payload)
 	resp.status_code = 200
 	return resp
+
+@site.route("/site/sessions/<int:id>/<int:page>")
+def get_sessions(id, page):
+	payload = {}
+	if request.method == 'GET':
+		devi = Device.query.filter_by(id=id).first()
+		if devi != None:
+			sessions = Session.query.filter_by(host=devi)\
+				.order_by(Session.date_initiated.desc())\
+				.paginate(page=page, per_page=10)
+
+			payload["sessions"] = []
+			payload["iter_pages"] = []
+
+			for sess in sessions.items:
+				sess_items = {}
+				sess_items["id"] = sess.id
+
+				sess_items["duration"] = sess.duration
+				sess_items["power_used"] = sess.power_used
+				sess_items["amount_paid"] = sess.amount_paid
+
+				sess_items["date_initiated_year"] = sess.date_initiated.year
+				sess_items["date_initiated_month"] = sess.date_initiated.month
+				sess_items["date_initiated_day"] = sess.date_initiated.day
+				sess_items["date_initiated_hour"] = sess.date_initiated.hour
+				sess_items["date_initiated_minute"] = sess.date_initiated.minute
+				sess_items["date_initiated_second"] = sess.date_initiated.second
+
+				sess_items["location"] = sess.location
+				sess_items["port"] = sess.port
+				sess_items["increment_size"] = sess.increment_size
+				sess_items["increments"] = sess.increments
+
+				payload["sessions"].append(sess_items)
+
+
+			for page_num in sessions.iter_pages(left_edge=1, right_edge=1, left_current=1, right_current=2):
+				if page_num:
+					payload["iter_pages"].append(page_num)
+				else:
+					payload["iter_pages"].append(0)
+
+			resp = jsonify(payload)
+			resp.status_code = 200
+			return resp
+		else:
+			resp = jsonify(payload)
+			resp.status_code = 400
+			return resp
+	else:
+		resp = jsonify(payload)
+		resp.status_code = 405
+		return resp
