f9a5aaeeb602778e2457bee769c9a85523ec54ca
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-10-15 20:36:18 -0700

    Fix config

--- CB_service/config.py
+++ CB_service/config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+	SECRET_KEY = "14c85b73853212ffbf2703072865d612"
+	SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('EMAIL_USER')
-	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+	MAIL_USERNAME = "testflaskmailserver@gmail.com"
+	MAIL_PASSWORD = "ForTheTest@123"
