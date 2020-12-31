8539f33aa5ffebe7de16f81e259ae86e7d92bbff
Christian Rodriguez <christianrodriguez795@yahoo.com>
2020-12-30 16:34:14 -0800

    Added csrf key

--- CB_service/config.py
+++ CB_service/config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY')
+	WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
