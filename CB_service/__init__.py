d88c6818ce7547cf4564ee8c4fbb4a2c0062be31
Christian Rodriguez <christianrodriguez795@yahoo.com>
2021-01-02 22:42:58 -0800

    Added import request

--- CB_service/__init__.py
+++ CB_service/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
